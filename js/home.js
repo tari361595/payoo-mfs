1b2849c9b9fecb1f44e8b46690f361b959bf3903
tari361595 <cmtariqulislam1994@gmail.com>
2024-10-30 10:18:36 +0900

    newBalance code done

--- js/home.js
+++ js/home.js
@@ -10,6 +10,14 @@ document.getElementById('btn-add-money').addEventListener('click',function(event
     // verified pin
     if(addPinInput ==='1234'){
         console.log('adding money to your account');
+        // get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+        // add addMoneyInput with balance
+        const addMoneyNmber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNmber + balanceNumber;
+        console.log(newBalance);
     }
     else{
         alert('failed to add money!please try again');
