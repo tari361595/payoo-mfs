fbaf169d92571e5f1621ca64314834e1be4cd785
tari361595 <cmtariqulislam1994@gmail.com>
2024-10-30 09:13:18 +0900

    added input money and pin

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log('home added');
+// first step add eventlistener
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    // get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    // get the pin number
+    const addPinInput = document.getElementById('input-pin-number').value;
+    console.log(addPinInput);
+
+})
